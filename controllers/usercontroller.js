18d340db4a7dce7013bf669250a969aaaee82d3b
nehakardam213 <nehachamankardam2000@gmail.com>
2024-09-13 18:46:10 +0530

    upload first version

---
+++ controllers/usercontroller.js
@@ -0,0 +1,21 @@
+const users = require('../users.json');
+function getAllUsers(req,res){
+    try{
+        res.json(users);
+    }catch(err){
+
+    }
+    function getUser(req,res){
+        try{
+         let id = parseInt(req.params.id)
+         let user = users.find((user)=>user.id ===id);
+         res.json(user)
+        }catch(err){
+
+        }
+    }
+}
+module.exports = {
+    getAllUsers,
+    getUser
+}
